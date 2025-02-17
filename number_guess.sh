4eb45d9fa35a1621388b4bd547f4e6c017a6eb55
Sony Hoang <sonyhoang53@gmail.com>
2025-02-17 18:35:08 +0000

    feat: add user acquisition script

--- number_guess.sh
+++ number_guess.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
+
+MAIN() {
+  echo "Enter your username:"
+  read USERNAME
+
+  GET_USERNAME=$($PSQL "select user_id from users where username=$GET_USERNAME")
+  if [[ -z $GET_USERNAME ]]
+  then
+    echo "Welcome, $USERNAME! It looks like this is your first time here."
+  else
+    echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
+}
