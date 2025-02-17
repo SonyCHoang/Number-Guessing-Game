cccda1607266bd924d8b0ccc578660cb04e67c54
Sony Hoang <sonyhoang53@gmail.com>
2025-02-17 21:56:54 +0000

    feat: add functionality to game

--- number_guess.sh
+++ number_guess.sh
@@ -6,14 +6,15 @@ MAIN() {
   echo "Enter your username:"
   read USERNAME
 
-  GET_USERNAME=$($PSQL "select user_id from users where username=$GET_USERNAME")
-  if [[ -z $GET_USERNAME ]]
+  GET_USERID=$($PSQL "select user_id from users where username='$USERNAME'")
+  if [[ -z $GET_USERID ]]
   then
     echo "Welcome, $USERNAME! It looks like this is your first time here."
-    INSERT_USERNAME=$($PSQL "insert into users(username) values($USERNAME)")
+    INSERT_USER=$($PSQL "insert into users(username, games_played) values('$USERNAME', 0)")
+    GET_USERID=$($PSQL "select user_id from users where username='$USERNAME'")
   else
-    GAMES_PLAYED=$($PSQL "select games_played from users where user_id=$GET_USERNAME")
-    BEST_GAME=$($PSQL "select best_game from users where user_id=$GET_USERNAME")
+    GAMES_PLAYED=$($PSQL "select games_played from users where user_id=$GET_USERID")
+    BEST_GAME=$($PSQL "select best_game from users where user_id=$GET_USERID")
     echo "Welcome back, $USERNAME! You have played $GAMES_PLAYED games, and your best game took $BEST_GAME guesses."
   fi
 
@@ -36,6 +37,17 @@ GAME() {
       then
         TRIES=$(($TRIES + 1))
         echo "You guessed it in $TRIES tries. The secret number was $SECRET_NUMBER. Nice job!"
+        GAMES_PLAYED=$(($GAMES_PLAYED + 1))
+        UPDATE_GAMES_PLAYED=$($PSQL "update users set games_played=$GAMES_PLAYED where user_id=$GET_USERID")
+        if [[ -z $BEST_GAME ]]
+        then
+          UPDATE_BEST_GAME=$($PSQL "update users set best_game=$TRIES where user_id=$GET_USERID")
+        else
+          if [[ $TRIES -lt $GET_TRIES ]]
+          then
+            UPDATE_BEST_GAME=$($PSQL "update users set best_game=$TRIES where user_id=$GET_USERID")
+          fi
+        fi
         CORRECT=true
     elif [[ $SECRET_NUMBER -gt $GUESS ]]
       then
@@ -47,3 +59,5 @@ GAME() {
     fi
   done
 }
+
+MAIN
